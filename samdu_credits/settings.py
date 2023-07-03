ba7d4bdc6f1f17c9728787e7a024765d61adcdf7
Asaf <asafmirzoev1404@gmail.com>
2023-07-03 15:14:13 +0500

    init

--- samdu_credits/settings.py
+++ samdu_credits/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-t!onn57n7-(nr2&+o_5jk@e##advy-ed$3_b_=6)@xwo)8wnk4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['credits.medow.uz', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -71,26 +71,23 @@ WSGI_APPLICATION = 'samdu_credits.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql', 
-#         'NAME': 'medowuz_credits',
-#         'USER': 'medowuz_medowuz',
-#         'PASSWORD': 'ON;w;4Du+zwm',
-#         'HOST': 'localhost',
-#         'PORT': 3306,
-#         'OPTIONS': {
-#             'sql_mode': 'traditional',
-#         }
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql', 
+        'NAME': 'creditsdb',
+        'USER': 'gold',
+        'PASSWORD': 'SuperGold_7',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
+}
 
 
 # Password validation
