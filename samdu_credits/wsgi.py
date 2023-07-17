3a73e360015aa3e5f7d9946142d954792eecc7bc
Asaf <asafmirzoev1404@gmail.com>
2023-07-17 11:09:39 +0500

    init

--- samdu_credits/wsgi.py
+++ samdu_credits/wsgi.py
@@ -5,8 +5,8 @@ from django.core.wsgi import get_wsgi_application
 
 from credits.utils import InitData, init_deadline
 
-InitData().run()
-init_deadline()
+# InitData().run()
+# init_deadline()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'samdu_credits.settings')
 
