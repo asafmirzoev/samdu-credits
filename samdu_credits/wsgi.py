e3943ba35984557c8d7b25544a2090ca9cc777a0
Asaf <asafmirzoev1404@gmail.com>
2023-11-07 11:45:51 +0500

    deadline

--- samdu_credits/wsgi.py
+++ samdu_credits/wsgi.py
@@ -3,9 +3,11 @@ import os
 import logging
 from django.core.wsgi import get_wsgi_application
 
+from credits.utils import init_deadline
 
 logging.basicConfig(filename='logs.log', level=logging.INFO)
 
+init_deadline()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'samdu_credits.settings')
 
