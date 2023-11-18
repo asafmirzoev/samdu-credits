2062f88c74c2b29de0565988f088a902f0c7586f
Asaf <asafmirzoev1404@gmail.com>
2023-11-18 11:07:44 +0500

    credits

--- credits/models.py
+++ credits/models.py
@@ -176,7 +176,7 @@ class Credit(models.Model):
     status = models.CharField(max_length=64, choices=CreditStatuses.choices, default=CreditStatuses.DEANERY_UPLOADED)
     active = models.BooleanField(default=True)
 
-    objects = CreditsManager()
+    # objects = CreditsManager()
 
     class Meta:
         ordering = ['student__name']
